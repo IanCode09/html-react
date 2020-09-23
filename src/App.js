094a2fc0c42385a4edecb06d8574c099d94d965c
IanCode09 <ianlombu@protonmail.com>
2020-09-24 06:50:29 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'assets/css/style.css'
+import HomePage from 'scenes/HomePage'
+function App() {
+  return (
+    <div className="App">
+      
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
